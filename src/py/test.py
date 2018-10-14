b7d5859ce578c2094658d6b8fc28c3196b8307c5
Richard G. Morrill <rmorrill2@fordham.edu>
2018-10-14 14:28:25 -0400

    submit.py now constructs .dat files

--- src/py/test.py
+++ src/py/test.py
@@ -12,13 +12,20 @@
 import sqlite3
 from sqlite3 import OperationalError
 import sqlExecFile;
+import skeleton_parser
 
 # r:                          runs sql from a file
-r=sqlExecFile.sqlExecFile
+r = sqlExecFile.sqlExecFile
+
+# parse:                      parses json into .dat files
+parse = skeleton_parser.parseJson
 
 # Set up database connection
 conn = sqlite3.connect('../../tmp/ebay_data.db')
 c = conn.cursor()
 
 # Run query
-r(c,'../sql/init.sql')
+r(c,'../sql/create.sql')
+
+for i in range (0,40):
+  parse('../../ebay_data/items-%d.json' % i)
