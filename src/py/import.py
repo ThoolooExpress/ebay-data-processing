0fd013e7b887ba9c7cbad834c5db8596ce07f05f
Richard G. Morrill <rmorrill2@fordham.edu>
2018-10-21 14:59:54 -0400

    We can now import users
    import script now works for users.  Will be made to work for
    other fields soon

---
+++ src/py/import.py
@@ -0,0 +1,217 @@
+# File:                         import.py
+# Purpose:                      imports the JSON files into the database
+# Note:                         partially based on prof's code
+
+# Current Status:               R. G. M. 2018-10-20 Currently only actually
+#                               handles users.  Will be extended to all fields
+#                               once it works for users
+
+# Comment Below is Prof's
+
+"""
+FILE: skeleton_parser.py
+------------------
+Skeleton parser for programming project 1. Has useful imports and
+functions for parsing, including:
+
+1) Directory handling -- the parser takes a list of eBay json files
+and opens each file inside of a loop. You just need to fill in the rest.
+2) Dollar value conversions -- the json files store dollar value amounts in
+a string like $3,453.23 -- we provide a function to convert it to a string
+like XXXXX.xx.
+3) Date/time conversions -- the json files store dates/ times in the form
+Mon-DD-YY HH:MM:SS -- we wrote a function (transformDttm) that converts to the
+for YYYY-MM-DD HH:MM:SS, which will sort chronologically in SQL.
+
+Your job is to implement the parseJson function, which is invoked on each file by
+the main function. We create the initial Python dictionary object of items for
+you; the rest is up to you!
+Happy parsing!
+"""
+
+import sys
+from json import loads
+from re import sub
+import sqlite3
+import sqlExecFile
+from contextlib import ExitStack
+
+r = sqlExecFile.sqlExecFile
+
+columnSeparator = "|"
+
+# Dictionary of months used for date transformation
+MONTHS = {'Jan':'01','Feb':'02','Mar':'03','Apr':'04','May':'05','Jun':'06',\
+'Jul':'07','Aug':'08','Sep':'09','Oct':'10','Nov':'11','Dec':'12'}
+
+"""
+Returns true if a file ends in .json
+"""
+def isJson(f):
+  return len(f) > 5 and f[-5:] == '.json'
+
+"""
+Converts month to a number, e.g. 'Dec' to '12'
+"""
+def transformMonth(mon):
+  if mon in MONTHS:
+    return MONTHS[mon]
+  else:
+    return mon
+
+"""
+Transforms a timestamp from Mon-DD-YY HH:MM:SS to YYYY-MM-DD HH:MM:SS
+"""
+def transformDttm(dttm):
+  dttm = dttm.strip().split(' ')
+  dt = dttm[0].split('-')
+  date = '20' + dt[2] + '-'
+  date += transformMonth(dt[0]) + '-' + dt[1]
+  return date + ' ' + dttm[1]
+
+"""
+Transform a dollar value amount from a string like $3,453.23 to XXXXX.xx
+"""
+
+def transformDollar(money):
+  if money == None or len(money) == 0 or money == "NULL":
+    return money
+    return sub(r'[^\d.]', '', money)
+
+def escapeQuote(string):
+  if string == None:
+    return string
+    return '\"' + sub(r'\"','\"\"',string) + '\"'
+
+"""
+Schema of Item table is
+Items (ItemID, SellerID, Name, Buy_Price, First_Bid, Currently,
+Number_of_Bids, Started, Ends, Description)
+"""
+# def parseItem(dictionary):
+#     with open("../../tmp/items.dat", "a") as f:
+#         item = []
+#         item.append(dictionary["ItemID"])
+#         item.append(dictionary["Seller"]["UserID"])
+#         item.append(escapeQuote(dictionary["Name"]))
+#         item.append(transformDollar(dictionary.get("Buy_Price", "NULL")))
+#         item.append(transformDollar(dictionary["First_Bid"]))
+#         item.append(transformDollar(dictionary["Currently"]))
+#         item.append(dictionary["Number_of_Bids"])
+#         item.append(transformDttm(dictionary["Started"]))
+#         item.append(transformDttm(dictionary["Ends"]))
+#         item.append(escapeQuote(dictionary["Description"]))
+#         f.write("|".join(map(lambda s: s or "", item)))
+#         f.write("\n")
+
+def addUser(userID,rating,location,country,cur):
+  # Purpose:                            Adds a use to the database
+  # Precondition:                       cur must be a valid sqlite3 database
+  #                                     cursor, and the preceding arguments
+  #                                     must make up a valid user profile
+  cur.execute(
+    """INSERT OR IGNORE INTO 'user' (userID,rating,location,country)
+       VALUES (?,?,?,?);
+    """,
+    [userID,
+    rating,
+    location,
+    country])
+
+# END addUser
+
+
+"""
+Schema of User table is
+User (UserID, Rating, Location, Country)
+"""
+def parseUser(dictionary,cur):
+  bids = dictionary.get("Bids")
+  if bids != None:
+    for bid in bids:
+      bidder = bid["Bid"]["Bidder"]
+      userID = bidder["UserID"]
+      rating = bidder["Rating"]
+      location = escapeQuote(bidder.get("Location", "NULL"))
+      country = escapeQuote(bidder.get("Country", "NULL"))
+      addUser(userID,rating,location,country,cur)
+
+  userID = dictionary["Seller"]["UserID"]
+  rating = dictionary["Seller"]["Rating"]
+  location = escapeQuote(dictionary.get("Location", "NULL"))
+  country = escapeQuote(dictionary.get("Country", "NULL"))
+  addUser(userID,rating,location,country,cur)
+
+"""
+Schema of Categories table is
+Categories (ItemID, Category)
+"""
+# def parseCategory(dictionary):
+#     with open("../../tmp/category.dat", "a") as f:
+#         category = ["|".join([dictionary["ItemID"], c]) \
+#                     for c in dictionary.get("Category")]
+
+#         f.write("\n".join(category))
+#         f.write("\n")
+
+"""
+Schema of Bids table is
+Bids (ItemID, UserID, Time, Amount)
+"""
+# def parseBids(dictionary):
+#     with open("../../tmp/bids.dat", "a") as f:
+#         bids = dictionary.get("Bids")
+#         if bids != None:
+#             for bid in bids:
+#                 info = []
+#                 info.append(dictionary["ItemID"])
+#                 info.append(bid["Bid"]["Bidder"]["UserID"])
+#                 info.append(transformDttm(bid["Bid"]["Time"]))
+#                 info.append(transformDollar(bid["Bid"]["Amount"]))
+#                 f.write("|".join(info) + "\n")
+
+"""
+Parses a single json file. Currently, there's a loop that iterates over each
+item in the data set. Your job is to extend this functionality to create all
+of the necessary SQL tables for your database.
+"""
+def parseJson(json_file):
+  print("parseJson exec")
+  with ExitStack() as stack:
+    f = stack.enter_context(open(json_file, 'r'))
+    print("NOW PARSINg")
+    conn = stack.enter_context(sqlite3.connect("../../tmp/ebay-data.db"))
+    items = loads(f.read())['Items']
+    cur = conn.cursor()
+    r(cur,"../sql/create.sql")
+    cur.execute("BEGIN TRANSACTION;") 
+    for item in items:
+      """
+      TODO: traverse the items dictionary to extract information from the
+      given `json_file' and generate the necessary .dat files to generate
+      the SQL tables based on your relation design
+      """
+      # parseItem(item)
+      parseUser(item, cur)
+      # parseCategory(item)
+      # parseBids(item)
+
+    cur.execute("COMMIT;")
+
+"""
+Loops through each json files provided on the command line and passes each file
+to the parser
+"""
+def main(argv):
+
+  if len(argv) < 2:
+    print >> sys.stderr, 'Usage: python skeleton_json_parser.py <path to json files>'
+    sys.exit(1)
+      # loops over all .json files in the argument
+  for f in argv[1:]:
+    if isJson(f):
+      parseJson(f)
+      print("Success parsing " + f)
+
+if __name__ == '__main__':
+  main(sys.argv)
--- src/sql/create.sql
+++ src/sql/create.sql
@@ -3,21 +3,95 @@
 --Author:                 Richard G. Morrill
 --Created Date:           2018-10-14
 
---Current Status:         R.G.M. 2018-10-14 This is just a test schema, I'm
---                        only representing a very small portion of the data
---                        with this schema
+--Current Status:         R. G. M. 2018-10-20 Added tables to match ER diagram, 
+--                        but for now everything but user is commented out for
+--                        testing
 
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS item;
--- DROP TABLE IF EXISTS listing;
--- DROP TABLE IF EXISTS bid;
+-- Note about date/times: I decided to use UNIX times, because I'm familiar with
+--                        them.  This means that all date/time fields are
+--                        of type INTEGER, and record the number of seconds
+--                        since 1970-01-01 00:00:00 UTC (R.G.M. 2018-10-20)
 
-CREATE TABLE user (
-  user_id VARCHAR PRIMARY KEY,
-  rating INTEGER
+-- Note about currency:   In keeping with the way almost all financial software
+--                        works, I'm storing monetary values as cents in INTEGER
+--                        fields to avoid any potential for floating point
+--                        issues.  The conversion to decimal values must be
+--                        handled outside the database
+
+DROP TABLE IF EXISTS "inCategory";
+DROP TABLE IF EXISTS "category";
+DROP TABLE IF EXISTS "bids";
+DROP TABLE IF EXISTS "listing";
+DROP TABLE IF EXISTS "item";
+DROP TABLE IF EXISTS "user";
+
+CREATE TABLE "user" (
+  -- Stores each user
+
+  userID TEXT PRIMARY KEY, --     the user's unique username
+  rating INTEGER, --              the user's rating
+  location TEXT, --               the user's location.  note that there isn't
+  --                              any specific format or validation, this is
+  --                              actually just whatever the user decides to
+  --                              put in the box when they register
+  country TEXT --                 the user's country
 );
 
-CREATE TABLE item (
-  item_id INTEGER PRIMARY KEY,
-  name VARCHAR
-)
+-- CREATE TABLE "item" (
+--   -- Stores each unique item.  Even if the same item is listed by
+--   -- different sellers at different times, it is still the same item
+
+--   itemID INTEGER PRIMARY KEY, --  unique item identifier
+--   name TEXT, --                   the user-friendly name of the item
+--   description TEXT  --            a more complete description of the item
+-- );
+
+-- CREATE TABLE "listing" (
+--   -- Stores each listing, which "lists" a single "item", and is "posted"
+--   -- by a single "user"
+
+--   buyPrice INTEGER, --            buy it now price, in whole cents
+--   firstBid INTEGER, --            first bid price, in whole cents
+--   starts INTEGER, --              auction start date/time
+--   ends INTEGER, --                auction end date/time
+
+--   itemID INTEGER, --              the item being listed
+--   FOREIGN KEY(itemID) REFERENCES "item"(itemID),
+
+--   sellerUserID TEXT, --            the seller of this item
+--   FOREIGN KEY(sellerUserID) REFERENCES "user"(userID),
+
+--   listingID INTEGER PRIMARY KEY -- this is an sqlite3 alias for ROWID, this
+--   --                               makes it easier for other tables to refenerce
+--   --                               this one
+-- );
+
+-- CREATE TABLE "bids" (
+--   -- Records each bid, by one user on one item
+
+--   userID TEXT, --                 the user bidding on the item
+--   FOREIGN KEY (userID) REFERENCES "user"(userID),
+
+--   listingID INTEGER, --           the listing that has been bid on
+--   FOREIGN KEY (listingID) REFERENCES "listing"(listingID),
+
+--   "time" INTEGER, --              the date/time the bid was placed
+
+--   price INTEGER --                the bid price, in whole cents
+-- );
+
+-- CREATE TABLE category (
+--   -- Stores each category
+--   name TEXT UNIQUE, --            the name of the category
+--   catID INTEGER PRIMARY KEY --    sqlite3 alias for ROWID
+-- );
+
+-- CREATE TABLE inCategory (
+--   -- Records that an item is in a category
+--   itemID INTEGER,
+--   FOREIGN KEY(itemID) REFERENCES "item"(itemID),
+--   catID INTEGER,
+--   FOREIGN KEY(catID) REFERENCES "category"(catID),
+
+--   PRIMARY KEY (itemID, catID)
+-- )
